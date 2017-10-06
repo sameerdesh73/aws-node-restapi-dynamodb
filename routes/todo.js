9442772370591a2f6f0205dd2f67db503407f616
Sameer Deshmukh <sdeshmukh@mbp-sdeshmukh.wafs.prv>
2017-10-06 12:13:29 -0700

    updated todo router for REST

--- routes/todo.js
+++ routes/todo.js
@@ -10,24 +10,32 @@ const express        = require('express');
 
 //const todoController    = require('../controllers/todo');
 
-module.exports = router;
-
 // Create the express router object for todos
 var todoRouter = express.Router();
 
 // A GET to the root of a resource returns a list of that resource
-todoRouter.get("/", function(req, res) { });
+todoRouter.get("/", function(req, res) { 
+    res.send("todo: list")
+});
 
 // A POST to the root of a resource should create a new object
-todoRouter.post("/", function(req, res) { });
+todoRouter.post("/", function(req, res) { 
+    res.send("todo: Create new")
+});
 
 // We specify a param in our path for the GET of a specific object
-todoRouter.get("/:id", function(req, res) { });
+todoRouter.get("/:id", function(req, res) { 
+    res.send("todo: get one")
+});
 
 // Similar to the GET on an object, to update it we can PATCH
-todoRouter.patch("/:id", function(req, res) { });
+todoRouter.patch("/:id", function(req, res) { 
+    res.send("todo: update one");
+});
 
 // Delete a specific object
-todoRouter.delete('/:id', function(req, res) { });
+todoRouter.delete('/:id', function(req, res) { 
+    res.send("todo:  delete one");
+});
 
 module.exports = todoRouter;
