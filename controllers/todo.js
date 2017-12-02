e166c16132d95946fe6117e88d64f3a73fe4146b
Sameer Deshmukh <sdeshmukh@mbp-sdeshmukh.wafs.prv>
2017-12-01 16:06:52 -0800

    using exists instead of notempty under input validation

--- controllers/todo.js
+++ controllers/todo.js
@@ -58,7 +58,7 @@ exports.read = function(){
 exports.create = function(req, res, next){
     //throw new Error('sameer try error');
 
-    req.checkBody("taskname", "taskname can not be empty").notEmpty();
+    req.checkBody("taskname", "taskname is required").exists();
     req.checkBody("taskname", "taskname must be between 2 and 20 characters").isLength({min:2, max:20});
 
     var errors = req.validationErrors();
